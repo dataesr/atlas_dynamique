db2f508185c77d5abd08bda8c5fb9f77a8b45fc8
hfsllt <100839261+hfsllt@users.noreply.github.com>
2022-06-29 17:12:49 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './Carto2';
+import App from './Carto3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
